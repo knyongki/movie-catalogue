032d62440294185430fe156359b35f7e62148c33
knyongki <yyongkisetiawan@gmail.com>
2024-10-22 22:07:57 +0700

    web socket imlemented

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -7,6 +7,7 @@ const CONFIG = {
   DATABASE_NAME: 'movie-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_SOTRAGE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
 };
 
 export default CONFIG;
