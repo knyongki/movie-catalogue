e9b207f6f057bce6cf0e303103cef7bbd11e0398
knyongki <yyongkisetiawan@gmail.com>
2024-10-22 21:58:05 +0700

    add liked page

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -21,4 +22,25 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://api.themoviedb.org/3/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'themoviedb-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://image.tmdb.org/t/p/w500/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'themoviedb-image-api',
+          },
+        },
+      ],
+    }),
+  ],
 });
