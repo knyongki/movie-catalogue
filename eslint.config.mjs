977cb352eeed56d2978e548f4189b11671caafb3
knyongki <yyongkisetiawan@gmail.com>
2024-11-17 13:50:23 +0700

    test implemented

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   daStyle,
+  jestPlugin.configs.recommended,
 ];
---
+++ tests/likeMovie.test.js
@@ -0,0 +1,17 @@
+import 'fake-indexeddb/auto';
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiatior';
+
+describe('Liking A Movie', () => {
+  it('should show the like button when the movie has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+  });
+});
