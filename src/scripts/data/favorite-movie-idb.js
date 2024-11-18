f79e685889b1c20ba4b416e81a43066ba8542008
knyongki <yyongkisetiawan@gmail.com>
2024-11-18 21:27:38 +0700

    like movie test

--- src/scripts/data/favorite-movie-idb.js
+++ src/scripts/data/favorite-movie-idb.js
@@ -1,29 +1,29 @@
 import { openDB } from 'idb';
 import CONFIG from '../globals/config';
 
-const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORAGE_NAME } = CONFIG;
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
 
 const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
   upgrade(database) {
-    database.createObjectStore(OBJECT_STORAGE_NAME, { keyPath: 'id' });
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
   },
 });
 
 const FavoriteMovieIdb = {
   async getMovie(id) {
-    return (await dbPromise).get(OBJECT_STORAGE_NAME, id);
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
   },
 
   async getAllMovies() {
-    return (await dbPromise).getAll(OBJECT_STORAGE_NAME);
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
   },
 
   async putMovie(movie) {
-    return (await dbPromise).put(OBJECT_STORAGE_NAME, movie);
+    return (await dbPromise).put(OBJECT_STORE_NAME, movie);
   },
 
   async deleteMovie(id) {
-    return (await dbPromise).delete(OBJECT_STORAGE_NAME, id);
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
   },
 };
 
--- tests/likeMovie.test.js
+++ tests/likeMovie.test.js
@@ -1,5 +1,6 @@
 import 'fake-indexeddb/auto';
 import LikeButtonInitiator from '../src/scripts/utils/like-button-initiatior';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
 
 describe('Liking A Movie', () => {
   it('should show the like button when the movie has not been liked before', async () => {
@@ -13,5 +14,37 @@ describe('Liking A Movie', () => {
     });
 
     expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+  });
+
+  it('should not show the unlike button when the movie has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[arail-label="unlike this movie"]')).toBeFalsy();
+  });
+
+  it('should be able to like the movie', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+ 
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+ 
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+ 
+    // Memastikan film berhasil disukai
+    const movie = await FavoriteMovieIdb.getMovie(1);
+    expect(movie).toEqual({ id: 1 });
+ 
+    await FavoriteMovieIdb.deleteMovie(1);
   });
 });
